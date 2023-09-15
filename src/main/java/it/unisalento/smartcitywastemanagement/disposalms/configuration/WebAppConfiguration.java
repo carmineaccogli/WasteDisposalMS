8db4fcd333a97183b4db109ec15df42fb34a64b5
carmineaccogli <112572172+carmineaccogli@users.noreply.github.com>
2023-09-15 19:00:44 +0200

    Changed CORS Policy

--- src/main/java/it/unisalento/smartcitywastemanagement/disposalms/configuration/WebAppConfiguration.java
+++ src/main/java/it/unisalento/smartcitywastemanagement/disposalms/configuration/WebAppConfiguration.java
@@ -9,6 +9,6 @@ public class WebAppConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("GET","POST");
+        registry.addMapping("/**").allowedOrigins("http://disposalManagementService:8083").allowedMethods("GET","POST");
     }
 }
